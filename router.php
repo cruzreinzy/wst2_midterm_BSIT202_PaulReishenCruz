e77f3aa9e276cd65546a6adb94fae8e655e609d2
Reishen Cruz <reinbackupacc@gmail.com>
2023-05-15 09:49:45 +0800

    router connections

---
+++ router.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once('assets/classes/class.users.php');
+require_once('assets/classes/class.chats.php');
+
+$users = new Users;
+
+if($_GET['ref'] == 'regist'){
+    $users->regist($_POST['name'], $_POST['usn'], $_POST['pwd']);
+}
+
+if($_GET['ref'] == 'logins'){
+    $users->logins($_POST['logusn'], $_POST['logpwd']);
+}
+
+if($_GET['ref'] == 'send'){
+    $chats = new Chats;
+    $chats->send($_POST['message']);
+}
+
+if($_GET['ref'] == 'collect'){
+    $chats = new Chats;
+    ($chats->collect());
+}
+
+// if($_GET['ref'] == 'delete'){
+//     $chats = new Chats;
+//     $chats->delete($_POST);
+// }
