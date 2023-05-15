696c1485f81292e51f0fb02b029aca5de7daa304
Reishen Cruz <reinbackupacc@gmail.com>
2023-05-15 09:53:30 +0800

    classes chats and users query

---
+++ assets/classes/class.chats.php
@@ -0,0 +1,72 @@
+<?php
+
+class Chats{
+    
+    public function send ($message){
+
+        session_start();
+        
+        $pdo = new PDO('mysql:host=localhost;dbname=chatbase','root','');
+
+        $id = $_SESSION['auth'][0]['id'];
+        $message = $_POST['message'];
+        
+        $query = 'INSERT INTO chats(account_id,message) VALUES(:account_id,:message)';
+
+        $insert = $pdo->prepare($query);
+
+        $insert->bindValue('account_id',$id);
+        $insert->bindValue('message',$message);
+
+        $insert->execute();
+
+        echo 'success';
+    }
+
+
+    public function collect(){
+
+        session_start();
+
+        $id = $_SESSION['auth'][0]['id'];
+
+        $pdo = new PDO('mysql:host=localhost;dbname=chatbase','root','');
+
+        $query = 'SELECT accs.id, accs.name, chats.message FROM chats INNER JOIN accs ON chats.account_id = accs.id ORDER BY chats.timestamp ASC';
+
+        $stmt = $pdo->prepare($query);
+        
+        $stmt->execute();
+
+        $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $html = '';
+
+        foreach($data as $message){
+            
+            if($id == $message['id']){
+                $html.=
+                '<div id="messageko">
+                    <div class="mymsg bg-secondary bg-opacity-75 rounded-4 col-md-5 p-3 float-end mb-3">
+                        <p class="text-body text-opacity-75">Me</p>
+                        <p class="text-white">'.$message['message'].'</p>
+                        <button class="dltbtn btn btn-danger px-2.5 pt-1 float-end text-white" type="button">Unsend</button>
+                    </div>
+                </div>';
+            }else{
+                $html.=
+                '<div id="messagenya">
+                    <div class="bg-success bg-opacity-75 rounded-4 col-md-5 p-3 mb-3">
+                        <p class="text-white">'.$message['name'].'</p>
+                        <p class="text-body">'.$message['message'].'</p>
+                    </div>
+                </div>';
+            }
+            
+        }
+        echo $html;
+    }
+
+}
+
+
---
+++ assets/classes/class.users.php
@@ -0,0 +1,79 @@
+<?php
+
+class Users{
+
+    public function regist ($name,$usn,$pwd){
+    
+        $pdo = new PDO('mysql:host=localhost;dbname=chatbase','root','');
+
+        $username = $_POST['usn'];
+
+        $query = 'SELECT * FROM accs WHERE username=:username';
+
+        $check = $pdo->prepare($query);
+
+        $check->bindValue('username',$username);
+
+        $check->execute();
+
+        $records = $check->fetchAll(PDO::FETCH_ASSOC);
+
+        if(count($records) == 0){
+
+            $name = $_POST['name'];
+            $username = $_POST['usn'];
+            $password = $_POST['pwd'];
+
+            $query = 'INSERT INTO accs(name,username,password) VALUES(:name,:username,:password)';
+
+            $stmt = $pdo->prepare($query);
+
+            $stmt->bindValue('name',$name);
+            $stmt->bindValue('username',$username);
+            $stmt->bindValue('password',$pwd);
+            
+            $stmt->execute();
+
+            echo 'success';
+
+        }else{
+            echo 'username exist';
+        }
+
+    }
+
+
+    public function logins ($logusn,$logpwd){
+
+        session_start();
+        
+        $pdo = new PDO('mysql:host=localhost;dbname=chatbase','root','');
+
+        $username = $_POST['logusn'];
+        $password = $_POST['logpwd'];
+
+        $query = 'SELECT * FROM accs WHERE username=:username and password=:password';
+
+        $check = $pdo->prepare($query);
+
+        $check->bindValue('username',$username);
+        $check->bindValue('password',$password);
+
+        $check->execute();
+
+        $records = $check->fetchAll();
+
+        if(count($records) == 0){
+
+            echo 'invalid';
+
+        }else{
+
+            $_SESSION['auth'] = $records;
+            echo 'success';
+
+        }
+    }
+
+
+}
